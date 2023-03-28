d3384db3d16c9bec8ed6f31cfa40e9109fede1dc
ELGHACHI MOHAMED <Mohamed.Elghachi@4d.com>
2023-03-28 09:40:09 +0000

    lesson-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,19 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text(
             'My First App',
-            style: TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold),
-          ),
+            style: TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold),          ),
           centerTitle: true,
+          backgroundColor: Colors.green[600],
         ),
         body: Center(
             child: Text(
           'Body de l\'application',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24,fontFamily: 'Satisfy',letterSpacing: 2.0),
         )),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text('+'),
+          backgroundColor: Colors.green[600],
         ),
       ),
     ));
