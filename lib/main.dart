0fb6bd6fd78a02de2e2e76fbb2cead07aba82da4
ELGHACHI MOHAMED <Mohamed.Elghachi@4d.com>
2023-03-29 23:29:20 +0000

    lesson-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyFirstWidget());
+
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text(
             'My First App',
-            style: TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold),          ),
+            style: TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold),
+          ),
           centerTitle: true,
           backgroundColor: Colors.green[600],
         ),
         body: Center(
             child: Text(
-          'Body de l\'application',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24,fontFamily: 'Satisfy',letterSpacing: 2.0),
-        )),
+              'Body de l\'application',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  fontFamily: 'Satisfy',
+                  letterSpacing: 2.0),
+            )),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text('+'),
           backgroundColor: Colors.green[600],
         ),
       ),
-    ));
+    );
+  }
+}
